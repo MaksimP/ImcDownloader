c9d4f72e89f2620d165565074cf69124e917a3e0
MaksimP <maksimp86@mail.ru>
2015-09-15 19:28:58 +0300

    create TableModel

---
+++ src/main/java/imcdownloader/Model/TableModel.java
@@ -0,0 +1,65 @@
+package imcdownloader.Model;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import javax.swing.table.AbstractTableModel;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+
+
+public class TableModel extends AbstractTableModel {
+
+    private InputStream in;
+    private Workbook wr_book;
+    private Sheet sheet;
+    private int row_count;
+    private int column_count;
+
+    public TableModel() {
+
+    }
+
+    public TableModel(String name_file) throws IOException {
+        in = new FileInputStream(name_file);
+        wr_book = new HSSFWorkbook(in);
+        sheet = wr_book.getSheetAt(0);
+
+
+    }
+
+
+
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Iterator<Row> it_row = sheet.rowIterator();
+        while (it_row.hasNext()) {
+            it_row.next();
+            it_row.next();
+            row_count++;
+            Row row = it_row.next();
+            Iterator<Cell> it_cell = row.iterator();
+            while (it_cell.hasNext()) {
+                Cell cell = it_cell.next();
+                column_count++;
+            }
+        }
+        return null;
+    }
+}
