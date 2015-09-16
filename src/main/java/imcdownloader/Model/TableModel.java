4d08bb6d34ec0fe91cfecd63f655c82111dba0a0
MaksimP <maksimp86@mail.ru>
2015-09-16 19:19:04 +0300

    create TableModel

--- src/main/java/imcdownloader/Model/TableModel.java
+++ src/main/java/imcdownloader/Model/TableModel.java
@@ -1,17 +1,13 @@
 package imcdownloader.Model;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import javax.swing.table.AbstractTableModel;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Iterator;
 
 
 public class TableModel extends AbstractTableModel {
@@ -19,52 +15,30 @@ public class TableModel extends AbstractTableModel {
     private InputStream in;
     private Workbook wr_book;
     private Sheet sheet;
-    private int row_count;
-    private int column_count;
-
-    public TableModel() {
-
-    }
 
     public TableModel(String name_file) throws IOException {
         in = new FileInputStream(name_file);
         wr_book = new HSSFWorkbook(in);
         sheet = wr_book.getSheetAt(0);
-        //sheet.get
-
-
     }
 
-
+    @Override
+    public String getColumnName(int column) {
+        return sheet.getRow(3).getCell(column).toString();
+    }
 
     @Override
     public int getRowCount() {
-        return 0;
+        return sheet.getLastRowNum();
     }
 
     @Override
     public int getColumnCount() {
-        return 0;
+        return sheet.getRow(3).getLastCellNum();
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        int count_rows = sheet.getLastRowNum();
-        for (int i = 0; i < 10; i++) {
-            Row row = sheet.getRow(i);
-        }
-        /*Iterator<Row> it_row = sheet.rowIterator();
-        while (it_row.hasNext()) {
-            it_row.next();
-            it_row.next();
-            row_count++;
-            Row row = it_row.next();
-            Iterator<Cell> it_cell = row.iterator();
-            while (it_cell.hasNext()) {
-                Cell cell = it_cell.next();
-                column_count++;
-            }
-        }*/
-        return null;
+        return sheet.getRow(rowIndex).getCell(columnIndex);
     }
 }
